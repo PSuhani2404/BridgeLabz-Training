451a32eb0a898ff18deb5d958af361a035e24b61
PSuhani2404 <suhani.pawar04@gmail.com>
2026-01-26 22:53:52 +0530

    [Suhani] : added string utils program

---
+++ java-collection-practice/gcr-codebase/javacollectionpractice/src/com/junit/stringutils/StringUtils.java
@@ -0,0 +1,18 @@
+package com.junit.stringutils;
+
+public class StringUtils
+{
+
+	    public String reverse(String str) {
+	        return new StringBuilder(str).reverse().toString();
+	    }
+
+	    public boolean isPalindrome(String str) {
+	        String reversed = reverse(str);
+	        return str.equals(reversed);
+	    }
+
+	    public String toUpperCase(String str) {
+	        return str.toUpperCase();
+	    }
+}
