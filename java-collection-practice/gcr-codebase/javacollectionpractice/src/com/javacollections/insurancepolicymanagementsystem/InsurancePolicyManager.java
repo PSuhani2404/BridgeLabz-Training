16229b259a8c3a5a1561ee2aeeb7856c77c6ecf5
PSuhani2404 <suhani.pawar04@gmail.com>
2026-01-26 09:54:09 +0530

    [Suhani] : modified insurance manager program

--- java-collection-practice/gcr-codebase/javacollectionpractice/src/com/javacollections/insurancepolicymanagementsystem/InsurancePolicyManager.java
+++ java-collection-practice/gcr-codebase/javacollectionpractice/src/com/javacollections/insurancepolicymanagementsystem/InsurancePolicyManager.java
@@ -1,36 +1,101 @@
 package com.javacollections.insurancepolicymanagementsystem;
-import java.util.ArrayList;
-import java.util.Set;
+import java.time.LocalDate;
+import java.util.*;
 public class InsurancePolicyManager 
 {
-		private static ArrayList<Integer> getUnion(Set<Integer> set1, Set<Integer> set2)
-		{
-			ArrayList<Integer> result = new ArrayList<>(set1);
-			
-			for(int i : set2) 
-			{
-				if(!set1.contains(i)) result.add(i);
-			}
-			return result;
-		}
-		
-		private static ArrayList<Integer> getIntersection(Set<Integer> set1, Set<Integer> set2)
-		{
-			ArrayList<Integer> result = new ArrayList<>();
-			
-			for(int i : set2) 
-			{
-				if(set1.contains(i)) result.add(i);
-			}
-			return result;
-		}
-		
-		public static void main(String[] args) 
-		{
-			Set<Integer> set1 = Set.of(1,2,3);
-			Set<Integer> set2 = Set.of(5,4,3);
-			
-			System.out.println(getUnion(set1, set2));
-			System.out.println(getIntersection(set1, set2));
-		}
+	    Map<Integer, Policy> policyMap = new HashMap<>();
+	    Map<Integer, Policy> insertionOrderMap = new LinkedHashMap<>();
+	    TreeMap<LocalDate, List<Policy>> expiryMap = new TreeMap<>();
+
+	    public void addPolicy(Policy policy) {
+	        policyMap.put(policy.policyNumber, policy);
+	        insertionOrderMap.put(policy.policyNumber, policy);
+
+	        if(!expiryMap.containsKey(policy.expiryDate)) expiryMap.put(policy.expiryDate, new ArrayList<>());
+	        expiryMap.get(policy.expiryDate).add(policy);
+	    }
+
+	    Policy getPolicy(int policyNumber) {
+	        return policyMap.get(policyNumber);
+	    }
+
+	    public void policiesExpiringSoon() {
+	        LocalDate next30 = LocalDate.now().plusDays(30);
+
+	        for(int key : policyMap.keySet()) {
+	        	Policy policy = policyMap.get(key);
+	        	if(policy.expiryDate.isBefore(next30)) System.out.println(policy);
+	        }
+	    }
+
+	    public void policiesByHolder(String name) {
+	        for (Policy p : policyMap.values()) {
+	            if (p.policyHolderName.equalsIgnoreCase(name)) System.out.println(p);
+	        }
+	    }
+
+	    public void removeExpiredPolicies() {
+	    	List<LocalDate> toRemoveDates = new ArrayList<>();
+	        for(LocalDate key : expiryMap.keySet()) {
+	        	if(LocalDate.now().isBefore(key)) break;
+	        	toRemoveDates.add(key);
+	        }
+	        
+	        for(LocalDate date : toRemoveDates) expiryMap.remove(date);
+	        
+	        List<Integer> toRemoveKeys = new ArrayList<>();
+	        for(int key : policyMap.keySet()) {
+	        	if(policyMap.get(key).expiryDate.isBefore(LocalDate.now())) {
+	        		toRemoveKeys.add(key);
+	        	}
+	        }
+	        
+	        for(int key : toRemoveKeys) {
+	        	policyMap.remove(key);
+	    		insertionOrderMap.remove(key);
+	        }
+	    }
+	    
+	    public static void main(String[] args) {
+	        InsurancePolicyManager manager = new InsurancePolicyManager();
+
+	        manager.addPolicy(new Policy(101, "John", LocalDate.now().plusDays(10), "Health"));
+	        manager.addPolicy(new Policy(102, "Rick", LocalDate.now().plusDays(40), "Car"));
+	        manager.addPolicy(new Policy(103, "Harry", LocalDate.now().minusDays(2), "Life"));
+
+	        System.out.println("Get Policy 101:");
+	        System.out.println(manager.getPolicy(101));
+
+	        System.out.println("Policies expiring in 30 days:");
+	        manager.policiesExpiringSoon();
+
+	        System.out.println("Policies for Rahul:");
+	        manager.policiesByHolder("Rahul");
+
+	        System.out.println("Removing expired policies");
+	        manager.removeExpiredPolicies();
+
+	        System.out.println("Remaining Policies:");
+	        for (Policy p : manager.policyMap.values()) {
+	            System.out.println(p);
+	        }
+	    }
+	}
+
+	class Policy {
+		int policyNumber;
+	    String policyHolderName;
+	    LocalDate expiryDate;
+	    String policyType;
+
+	    public Policy(int policyNumber, String policyHolderName, LocalDate expiryDate, String policyType) {
+	        this.policyNumber = policyNumber;
+	        this.policyHolderName = policyHolderName;
+	        this.expiryDate = expiryDate;
+	        this.policyType = policyType;
+	    }
+
+	    public String toString() {
+	        return policyNumber + " " + policyHolderName + " " + expiryDate + " " + policyType;
+	    }
 }
