6307f090abb1518bbbee82a3c91e618168f02acb
PSuhani2404 <suhani.pawar04@gmail.com>
2026-02-01 11:47:54 +0530

    [Suhani] : added hospital patient ID printing using method reference program

---
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingPractice/src/com/lambda/methodreference/hospital/Patient.java
@@ -0,0 +1,26 @@
+package com.lambda.methodreference.hospital;
+
+public class Patient 
+{
+	    private String patientId;
+	    private String name;
+	    private int age;
+
+	    // constructor
+	    public Patient(String patientId, String name, int age) {
+	        this.patientId = patientId;
+	        this.name = name;
+	        this.age = age;
+	    }
+
+	    // getter for ID (used in method reference)
+	    public String getPatientId() {
+	        return patientId;
+	    }
+
+	    // good string representation
+	    @Override
+	    public String toString() {
+	        return "Patient[ID=" + patientId + ", Name=" + name + ", Age=" + age + "]";
+	    }
+}	
